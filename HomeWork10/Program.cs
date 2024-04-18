0112c2ac2986d3064030d0ef4dbf7ef56ea88b65
Matveykylak <matveykylak@gmail.com>
2024-04-18 22:41:17 +0300

    Zero Task

--- HomeWork10/Program.cs
+++ HomeWork10/Program.cs
@@ -1,1 +1,19 @@
 ﻿
+using System.Collections;
+
+//Zero task
+
+/*try
+{
+    ArrayList list = new ArrayList();
+    object s = list[18];
+}
+catch(System.ArgumentOutOfRangeException ex)
+{
+    Console.WriteLine(ex.GetType());
+}
+finally
+{
+    Console.WriteLine("Zero Task");
+}*/
+
