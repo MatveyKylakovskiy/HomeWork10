b6eb351b6dda154630064afcdd217360db037b77
Matveykylak <matveykylak@gmail.com>
2024-04-20 17:21:08 +0300

    First task

--- HomeWork10/Program.cs
+++ HomeWork10/Program.cs
@@ -1,6 +1,7 @@
-﻿
+﻿using HomeWork10.Second_Task;
 using HomeWork10.ZeroTask;
-
+using HomeWork8.SecondTask;
+/*
 //Zero Task 1
 try
 {
@@ -16,3 +17,21 @@ ZeroTask.PointTwo();
 
 //Zero Task 3
 ZeroTask.PointThree();
+*/
+
+
+//First Task
+
+var goods = new Goods("bread", 2, new DateTime(2024, 04, 10), 3);
+var goods1 = new Goods("beer", 4, new DateTime(2024, 04, 10), 90);
+var goods2 = new Goods("potato", 0.15, new DateTime(2024, 02, 10), 300);
+
+var batch = new Batch(goods, 4);
+var batch1 = new Batch(goods1, 7);
+var batch2 = new Batch(goods2, 1000);
+
+
+List<Product> prods = new List<Product> { goods, goods1, goods2, batch, batch1, batch2 };
+
+WorkerWithProducts.FirstTask(prods);
+
---
+++ HomeWork10/WorkerWithProducts/WorkerWithProducts.cs
@@ -0,0 +1,53 @@
+﻿using HomeWork8.SecondTask;
+
+namespace HomeWork10.Second_Task
+{
+    public static class WorkerWithProducts
+    {
+        public static void FirstTask(List<Product> products)
+        {
+            foreach (Product product in products)
+            {
+                product.GetInfo();
+            }
+
+            products[0].Price = 100;
+
+            products.RemoveAt(products.Count - 1);
+
+            Product[] productsArray1 = products.ToArray();
+            Product[] productsArray2 = new Product[products.Count];
+            Product[] productsArray3 = new Product[products.Count];
+
+            for (int i = 0; i < products.Count; i++)
+            {
+                productsArray2[i] = products[i];
+            }
+
+            products.CopyTo(productsArray3);
+
+            products.Clear();
+        }
+
+
+
+    }
+}
+
+
+
+/*Задание 1:
+а) Создать динамический массив, содержащий объекты класса Product
+б) Распечатать его содержимое используя for each.
+в) Изменить цену одного продукта на 100.
+г) Удалить последний продукт.
+е) Получить массив содержащий продукты из коллекции тремя способами и вывести на
+консоль.
+ж) Удалить все продукты.*/
+
+
+/*
+Задание 2:
+Создать SortedSet содержащий объекты Product. Содержимое данной коллекции
+распечатать с помощью for each.
+*/
