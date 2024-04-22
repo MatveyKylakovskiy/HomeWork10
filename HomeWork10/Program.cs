b34cb3e85d51898f76f97a4378bc32517626fb03
Matveykylak <matveykylak@gmail.com>
2024-04-22 15:39:10 +0300

    Final

--- HomeWork10/Program.cs
+++ HomeWork10/Program.cs
@@ -1,4 +1,4 @@
-﻿using HomeWork10.Second_Task;
+﻿using HomeWork10.WorkerWithProducts;
 using HomeWork10.ZeroTask;
 using HomeWork8.SecondTask;
 /*
@@ -23,19 +23,66 @@ ZeroTask.PointThree();
 //First Task
 
 var goods = new Goods("bread", 2, new DateTime(2024, 04, 10), 3);
-var goods1 = new Goods("beer", 4, new DateTime(2024, 04, 10), 90);
-var goods2 = new Goods("potato", 0.15, new DateTime(2024, 02, 10), 300);
+var goods1 = new Goods("beer", 500, new DateTime(2024, 04, 10), 90);
+var goods2 = new Goods("aa", 1000, new DateTime(2024, 02, 10), 300);
+var goods3 = new Goods("ss", 1000, new DateTime(2024, 02, 10), 300);
+var goods4 = new Goods("dd", 222, new DateTime(2024, 02, 10), 300);
+var goods5 = new Goods("gg", 500, new DateTime(2024, 02, 10), 300);
+var goods6 = new Goods("pd", 0.15, new DateTime(2024, 02, 10), 300);
+var goods7 = new Goods("bb", 0.15, new DateTime(2024, 02, 10), 300);
 
-var batch = new Batch(goods, 4);
-var batch1 = new Batch(goods1, 7);
-var batch2 = new Batch(goods2, 1000);
+var batch = new Batch(goods, 1);
+var batch1 = new Batch(goods1, 56);
+var batch2 = new Batch(goods2, 4);
+var batch3 = new Batch(goods3, 1);
+var batch4 = new Batch(goods4, 45);
+var batch5 = new Batch(goods5, 6);
+var batch6 = new Batch(goods6, 71);
+var batch7 = new Batch(goods7, 1);
 
 
-List<Product> prods = new List<Product> { goods, goods1, goods2, batch, batch1, batch2 };
+List<Product> prods = new List<Product> {batch, batch1, batch2, batch3, batch4, batch5, batch6, batch7 };
 
 //First Task
 //WorkerWithProducts.GetListOfProducts(prods);
 
+//Second Task 
 
-//WorkerWithProducts.SortedSet(prods);
-prods.Sort();
+/*try
+{
+    foreach (var product in prods)
+    {
+        product.GetInfo();
+    }
+
+    Console.WriteLine();
+
+    WorkerWithProducts.SortedSet(prods);
+
+    foreach (var product in prods)
+    {
+        product.GetInfo();
+    }
+}
+catch(ArgumentException ex)
+{
+    Console.WriteLine(ex.Message);
+}*/
+
+//Third task
+
+//WorkerWithProducts.FindsExpensive(prods);
+
+//Fourts task
+try
+{
+    Dictionary<string, Product> products =  WorkerWithProducts.Print1(prods);
+    WorkerWithProducts.Print2(products);
+    WorkerWithProducts.Print3(products);
+}
+catch(ArgumentException ex)
+{
+    Console.WriteLine(ex.Message);  
+}
+
+
--- HomeWork10/WorkerWithProducts/WorkerWithProducts.cs
+++ HomeWork10/WorkerWithProducts/WorkerWithProducts.cs
@@ -1,14 +1,12 @@
 ﻿using HomeWork8.SecondTask;
-using System.Xml.Linq;
-using System;
 
-namespace HomeWork10.Second_Task
+namespace HomeWork10.WorkerWithProducts
 {
     public static class WorkerWithProducts
-    {   
+    {
         //First Task
         public static void GetListOfProducts(List<Product> products)
-        {   
+        {
 
             foreach (Product product in products)
             {
@@ -39,6 +37,99 @@ namespace HomeWork10.Second_Task
             products.Sort();
         }
 
+        //Third task
+        public static void FindsExpensive(List<Product> products)
+        {
+            List<Product> expensiveList = new List<Product>();
+
+            foreach (Product product in products)
+            {
+                if (product.Price > 300)
+                {
+                    expensiveList.Add(product);
+                }
+                product.GetInfo();
+            }
+
+            Console.WriteLine();
+
+            foreach (Product product in expensiveList)
+            {
+                product.GetInfo();
+            }
+
+            Console.WriteLine("Min value");
+
+            Product minValue = expensiveList[0];
+
+            for (int i = 1; i < expensiveList.Count; i++)
+            {
+                if (minValue.Price > expensiveList[i].Price)
+                {
+                    minValue = expensiveList[i];
+                }
+            }
+            minValue.GetInfo();
+
+        }
+
+        //Four task
+        public static Dictionary<string, Product> Print1(List<Product> products)
+        {
+            Dictionary<string, Product> _products = new Dictionary<string, Product>();
+            
+            foreach (Product product in products)
+            {
+                _products.Add(product.Name, product);
+            }
+
+            foreach (var product in _products)
+            {
+                Console.WriteLine($"Name = {product.Key}, Price = {product.Value.Price}, Count = {product.Value.Quantity}");
+            }
+            return _products;
+        }
+
+        public static void Print2(Dictionary<string, Product> products)
+        {
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Name = {product.Key}");
+            }
+
+        }
+
+        public static void Print3(Dictionary<string, Product> products)
+        {
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Count = {product.Value.Quantity}");
+            }
+
+        }
+
+        //Fifts task
+        public static Dictionary<string, Product> ListToDictionary(List<Product> products)
+        {
+            Dictionary<string, Product> _products = new Dictionary<string, Product>();
+
+            foreach (Product product in products)
+            {
+                _products.Add(product.Name, product);
+            }
+
+            return _products;
+        }
+
+        public static List<Product> DictionaryToKist(Dictionary<string, Product> products)
+        {
+            List<Product> _products = new List<Product>();
+            foreach(var product in products)
+            {
+                _products.Add(product.Value);
+            }
+            return _products;
+        }
     }
 }
 
@@ -59,3 +150,21 @@ namespace HomeWork10.Second_Task
 Создать SortedSet содержащий объекты Product. Содержимое данной коллекции
 распечатать с помощью for each.
 */
+
+/*
+Задание 3:
+Создать коллекцию, содержащую объекты Product. Написать метод, который перебирает
+элементы коллекции и проверяет цену продуктов. Если цена продукта больше 300
+рублей, продукт перемещается в другую коллекцию.*/
+
+/*Задание 4:
+Создайте Dictionary, содержащий пары значений - имя продукта и количестов единиц
+продукта (класс Product).
+Перебрать и распечатать пары значений в формате "Name = abc, Price = 15, Count = 5"
+Перебрать и распечатать набор из имен продуктов
+Перебрать и распечатать значения количества единиц продуктов.
+Для каждого перебора создать свой метод.*/
+
+/*Задание 5:
+На основе задания 4 необходимо написать преобразование Dictionary в List.
+На основе задания 3 необходимо написать преобразование List в Dictionary.*/
